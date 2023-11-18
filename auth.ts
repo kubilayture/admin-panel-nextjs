f58f07e4ccfc1c33bc807e9d56ef8d1b88de247e
Kubilay Türe <kubilayture@gmail.com>
2023-11-19 00:23:43 +0300

    login action

--- auth.ts
+++ auth.ts
@@ -1,32 +1,39 @@
 import NextAuth from "next-auth"
 import CredentialsProvider from "next-auth/providers/credentials"
 import { connectToDB } from './app/lib/utils'
+import { authConfig } from './auth.config'
 import { User } from './app/lib/models'
 import bcrypt from 'bcrypt'
 
 const login = async (credentials: any) => {
   try {
     await connectToDB()
-    const user = await User.findOne({
-      username: credentials.username
-    })
-    if (!user) {
-      throw new Error('Wrong credentials!')
-    }
-    const isPasswordCorrect = await bcrypt.compare(credentials.password, user.password)
+    const user = await User.findOne({ username: credentials.username })
+    console.log(user)
 
-    if (!isPasswordCorrect) {
-      throw new Error('Wrong credentials!')
-    }
+    console.log(credentials)
+
+    if (!user) throw new Error("Wrong credentials!!!")
+
+    console.log(credentials.password)
+    console.log(user.password)
+
+    const isPasswordCorrect = await bcrypt.compare(
+      credentials.password,
+      user.password
+    )
+
+    if (!isPasswordCorrect) throw new Error("Wrong credentials!")
+
     return user
-  }
-  catch (err: any) {
+  } catch (err) {
     console.log(err)
-    throw new Error('Failed to login')
+    throw new Error("Failed to login!")
   }
 }
 
 export const { signIn, signOut, auth } = NextAuth({
+  ...authConfig,
   providers: [
     CredentialsProvider({
       async authorize(credentials) {
